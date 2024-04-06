80ae8fe0acea013bde6441704fae4ffe69b9012d
Max <makasyslar@gmail.com>
2024-04-06 23:26:46 +0300

    Edit header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,13 @@ import { ButtonOrLink } from "@/ui";
 import st from "./styles.module.scss";
 import { Link } from "react-router-dom";
 import { LogoIcon } from "@/ui/icons";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 const Header = () => {
 	return (
 		<header className={st.header}>
 			<div className={`${st.wrapper} container`}>
+        <BurgerMenu/>
 				<Link to="#">
 					<LogoIcon size="big" />
 				</Link>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
+export { default as BurgerMenu } from "./BurgerMenu/BurgerMenu";
