df7ec606b96cce30d18d7378afdb130797061c38
Max <makasyslar@gmail.com>
2024-04-06 18:46:28 +0300

    Edit Header components and styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
 import { ButtonOrLink } from "@/ui";
 import st from "./styles.module.scss";
+import { Link } from "react-router-dom";
+import { LogoIcon } from "@/ui/icons";
 
 const Header = () => {
 	return (
 		<header className={st.header}>
-			<a href="#"></a>
-			<ButtonOrLink className="">My account</ButtonOrLink>
+			<div className={`${st.wrapper} container`}>
+				<Link to="#">
+					<LogoIcon size="big" />
+				</Link>
+				<ButtonOrLink className="">My account</ButtonOrLink>
+			</div>
 		</header>
 	);
 };
