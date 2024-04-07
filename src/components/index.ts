d9dcbddbdb310acafff12b44392a3bde75972a5f
Max <makasyslar@gmail.com>
2024-04-07 22:25:41 +0300

    Create DropDown component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
+export { default as BurgerMenu } from "./BurgerMenu/BurgerMenu";
+export { default as Comment } from "./Comment/Comment";
+export { default as CorporateInfoBlock } from "./CorporateInfoBlock/CorporateInfoBlock";
+export { default as DropDown } from "./DropDown/DropDown";
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
-export { default as BurgerMenu } from "./BurgerMenu/BurgerMenu";
 export { default as Hero } from "./Hero/Hero";
-export { default as Comment } from "./Comment/Comment";
 export { default as SwiperSlider } from "./SwiperSlider/SwiperSlider";
-export { default as CorporateInfoBlock } from "./CorporateInfoBlock/CorporateInfoBlock";
+
