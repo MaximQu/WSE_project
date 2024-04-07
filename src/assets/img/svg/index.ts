57a74bb96f7fa879de3f8f339f69b2a4e49174a3
end garv <skydorw@gmail.com>
2024-04-07 18:01:14 +0300

    Add images

---
+++ src/assets/img/svg/index.ts
@@ -0,0 +1,7 @@
+import analiticsSVG from "./analytics.svg";
+import confidenceSVG from "./confidence.svg";
+import contactsSVG from "./contacts.svg";
+import highFiveSVG from "./high_five.svg";
+import pazzleSVG from "./pazzle.svg";
+
+export { analiticsSVG, confidenceSVG, contactsSVG, highFiveSVG, pazzleSVG };
