5f00c4e3c7eb24cbf07e508b56288299be646f0c
end garv <skydorw@gmail.com>
2024-04-09 21:13:19 +0300

    Create trading chart widgets

---
+++ src/ui/Widgets/TradingViewWidgets.tsx
@@ -0,0 +1,80 @@
+import {
+	CopyrightStyles,
+	Ticker,
+	TickerTape,
+	AdvancedRealTimeChart,
+	SymbolInfo,
+	Screener,
+} from "react-ts-tradingview-widgets";
+
+import st from "./styles.module.scss";
+
+const styles: CopyrightStyles = {
+	parent: {
+		display: "none",
+	},
+	link: {
+		display: "none",
+	},
+	span: {
+		display: "none",
+	},
+};
+
+export const TickerTapeWidget = ({
+	isCompact = false,
+	className = "",
+}: {
+	isCompact?: boolean;
+	className?: string;
+}) => (
+	<div className={`${className} ${st.tickerTape}`}>
+		<TickerTape
+			copyrightStyles={styles}
+			displayMode={isCompact ? "compact" : "regular"}
+		/>
+	</div>
+);
+export const TickerWidget = ({ className = "" }: { className?: string }) => {
+	const content = [
+		{
+			proName: "FOREXCOM:SPXUSD",
+			title: "S&P 500",
+			id: 1,
+		},
+		{
+			proName: "FOREXCOM:SPXUSD",
+			title: "S&P 500",
+			id: 2,
+		},
+		{
+			proName: "FOREXCOM:SPXUSD",
+			title: "S&P 500",
+			id: 3,
+		},
+		{
+			proName: "FOREXCOM:SPXUSD",
+			title: "S&P 500",
+			id: 4,
+		},
+	];
+	return (
+		<div className={`${className} ${st.ticker}`}>
+			{content.map((item) => (
+				<div key={item.id}>
+					<Ticker
+						copyrightStyles={styles}
+						isTransparent
+						colorTheme="dark"
+						symbols={[{ proName: item.proName, title: item.title }]}
+					/>
+				</div>
+			))}
+		</div>
+	);
+};
+export const RealTimeChartWidget = () => (
+	<AdvancedRealTimeChart copyrightStyles={styles} />
+);
+export const SymbolInfoWidget = () => <SymbolInfo copyrightStyles={styles} />;
+export const ScreenerWidget = () => <Screener copyrightStyles={styles} />;
