b619623d429aa70a6f818506236635db86da01e8
end garv <skydorw@gmail.com>
2024-04-10 14:12:40 +0300

    Remove '/' from inner paths in Routes

--- src/App.tsx
+++ src/App.tsx
@@ -15,23 +15,23 @@ const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<MainLayout headerTheme="light" />}>
-				<Route path="/trade" element={<Trade />} />
-				<Route path="/sustainable" element={<Sustainable />} />
-				<Route path="/legal-notice" element={<LegalDisclaimer />} />
-				<Route path="/privacy-statement" element={<PrivacyStatement />} />
-				<Route path="/copyright" element={<Copyright />} />
-				<Route path="/auth/sign-up" element={<Registration />} />
+				<Route path="trade" element={<Trade />} />
+				<Route path="sustainable" element={<Sustainable />} />
+				<Route path="legal-notice" element={<LegalDisclaimer />} />
+				<Route path="privacy-statement" element={<PrivacyStatement />} />
+				<Route path="copyright" element={<Copyright />} />
+				<Route path="auth/sign-up" element={<Registration />} />
 				<Route path="*" element={<NotFound />} />
 			</Route>
 			<Route path="/" element={<MainLayout />}>
 				<Route index element={<Home />} />
-				<Route path="/market" element />
-				<Route path="/regulation" element />
-				<Route path="/affiliation" element />
-				<Route path="/private-markets" element />
-				<Route path="/why-wse" element />
-				<Route path="/contacts" element />
-				<Route path="/about-us" element>
+				<Route path="market" element />
+				<Route path="regulation" element />
+				<Route path="affiliation" element />
+				<Route path="private-markets" element />
+				<Route path="why-wse" element />
+				<Route path="contacts" element />
+				<Route path="about-us" element>
 					<Route path="company-profile" element />
 					<Route path="investor-relations" element />
 				</Route>
