60b0d0b531efa1a6a54e3bbe2727cd9f199f8465
end garv <skydorw@gmail.com>
2024-04-12 17:03:38 +0300

    Fix about-us routes: now can visit investor-relations

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,53 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import {
-  Affiliation,
-  CompanyProfile,
-  Contacts,
-  Copyright,
-  Home,
-  InvestorRelations,
-  LegalDisclaimer,
-  Market,
-  NotFound,
-  PrivacyStatement,
-  PrivateMarkets,
-  Registration,
-  Regulation,
-  Sustainable,
-  Trade,
-  WhyWSE,
+	Affiliation,
+	CompanyProfile,
+	Contacts,
+	Copyright,
+	Home,
+	InvestorRelations,
+	LegalDisclaimer,
+	Market,
+	NotFound,
+	PrivacyStatement,
+	PrivateMarkets,
+	Registration,
+	Regulation,
+	Sustainable,
+	Trade,
+	WhyWSE,
 } from "./pages";
 
 const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<MainLayout headerTheme="light" />}>
-				<Route path="trade" element={<Trade />} />
-				<Route path="sustainable" element={<Sustainable />} />
-				<Route path="market" element={<Market />} />
-				<Route path="regulation" element={<Regulation />} />
-				<Route path="private-markets" element={<PrivateMarkets />} />
-				<Route path="affiliation" element={<Affiliation />} />
-				<Route path="contacts" element={<Contacts />} />
-				<Route path="auth/sign-up" element={<Registration />} />
-				<Route path="about-us">
+			<Route path="/">
+				<Route element={<MainLayout headerTheme="light" />}>
+					<Route path="trade" element={<Trade />} />
+					<Route path="sustainable" element={<Sustainable />} />
+					<Route path="market" element={<Market />} />
+					<Route path="regulation" element={<Regulation />} />
+					<Route path="private-markets" element={<PrivateMarkets />} />
+					<Route path="affiliation" element={<Affiliation />} />
+					<Route path="contacts" element={<Contacts />} />
+					<Route path="auth/sign-up" element={<Registration />} />
+					<Route path="legal-notice" element={<LegalDisclaimer />} />
+					<Route path="privacy-statement" element={<PrivacyStatement />} />
+					<Route path="copyright" element={<Copyright />} />
+					<Route path="*" element={<NotFound />} />
+				</Route>
+				<Route element={<MainLayout />}>
+					<Route index element={<Home />} />
+					<Route path="why-wse" element={<WhyWSE />} />
+					<Route path="about-us" element></Route>
+				</Route>
+			</Route>
+			<Route path="about-us">
+				<Route element={<MainLayout headerTheme="light" />}>
 					<Route path="company-profile" element={<CompanyProfile />} />
 				</Route>
-				<Route path="legal-notice" element={<LegalDisclaimer />} />
-				<Route path="privacy-statement" element={<PrivacyStatement />} />
-				<Route path="copyright" element={<Copyright />} />
-				<Route path="*" element={<NotFound />} />
-			</Route>
-			<Route path="/" element={<MainLayout />}>
-				<Route index element={<Home />} />
-				<Route path="why-wse" element={<WhyWSE />} />
-				<Route path="about-us" element>
+				<Route element={<MainLayout />}>
 					<Route path="investor-relations" element={<InvestorRelations />} />
 				</Route>
 			</Route>
