31a84f1b53863617d1dbc5341b226397558374e0
end garv <skydorw@gmail.com>
2024-04-13 18:53:05 +0300

    small fixes

--- src/pages/Trade/Trade.tsx
+++ src/pages/Trade/Trade.tsx
@@ -1,12 +1,13 @@
-import { Clarification, SwiperSlider } from "@/components";
+import { Clarification } from "@/components";
+import TestimonialSlider from "@/components/TestimonialSlider/TestimonialSlider";
 import UniversalSection from "@/components/UniversalSection/UniversalSection";
 import { AdaptiveImg, MultipleButtons } from "@/ui";
 import {
 	RealTimeChartWidget,
 	TickerTapeWidget,
 } from "@/ui/Widgets/TradingViewWidgets";
-import st from "./styles.module.scss";
 import { wseDescription } from "./content";
+import st from "./styles.module.scss";
 
 const Trade = () => {
 	return (
