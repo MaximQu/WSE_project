cc4bf8f634408860df6ef9ebc03a5135f4f5ffd0
end garv <skydorw@gmail.com>
2024-04-16 00:51:59 +0300

    Fix imports

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,4 +1,6 @@
-export { default as ButtonOrLink } from "./ButtonOrLink/ButtonOrLink";
-export { default as Select } from "./Selectx/Select";
-export { default as MultipleButtons } from "./MultipleButtons/MultipleButtons";
 export { default as AdaptiveImg } from "./AdaptiveImg/AdaptiveImg";
+export { default as ButtonOrLink } from "./ButtonOrLink/ButtonOrLink";
+export { default as CountriesSelect } from "./CountriesSelect/CountriesSelect";
+export { default as CustomInput } from "./CustomInput/CustoInput";
+export { default as CustomPhoneInput } from "./CustomPhoneInput/CustomPhoneInput";
+export { default as MultipleButtons } from "./MultipleButtons/MultipleButtons";
